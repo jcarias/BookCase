36c61ea3ddac83b51485dfef2f251356b202800c
David Fernandes <david.v.fernandes@gmail.com>
2016-03-29 23:14:21 +0100

    Added Facebook SDK to the project
    https://developers.facebook.com/docs/android/getting-started/

--- app/src/main/java/pt/iscte/daam/bookcase/ProfileActivity.java
+++ app/src/main/java/pt/iscte/daam/bookcase/ProfileActivity.java
@@ -2,12 +2,49 @@ package pt.iscte.daam.bookcase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final String TAG = "ProfileActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_profile);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i(TAG, "Success!");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "Cancel!");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i(TAG, "error!");
+            }
+        });
     }
 }
