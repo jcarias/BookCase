5b85080c2159dc5b94dda425012d0d247168fcd6
jpafernandes <jpafernandes@gmail.com>
2016-04-18 16:34:25 +0100

    Adicionei o Package BookInfo com 3 classes: - book: com as informações acerca de um livro, tal e qual como vêm da API do goodreads - author: idem - user: está lá no xml do author, ainda não sei bem porquê nem para que serve, mas para manter consistencia de informação deixei ficar.

---
+++ app/src/main/java/pt/iscte/daam/bookcase/BookInfo/user.java
@@ -0,0 +1,18 @@
+package pt.iscte.daam.bookcase.BookInfo;
+
+/**
+ * Created by jpafernandes on 05/04/16.
+ */
+public class user {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+    //changed
+}
