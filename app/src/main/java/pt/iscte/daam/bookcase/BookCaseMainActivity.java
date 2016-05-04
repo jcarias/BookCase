c54d6a187c63c2eeffe84b9eff237ddeb992bec5
David Fernandes <david.v.fernandes@gmail.com>
2016-05-04 16:58:02 +0100

    Profile Picture in Menu Bar

--- app/src/main/java/pt/iscte/daam/bookcase/BookCaseMainActivity.java
+++ app/src/main/java/pt/iscte/daam/bookcase/BookCaseMainActivity.java
@@ -1,8 +1,12 @@
 package pt.iscte.daam.bookcase;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -12,12 +16,14 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -41,6 +47,7 @@ public class BookCaseMainActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    private Menu menu;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -85,8 +92,39 @@ public class BookCaseMainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.setProfileIcon();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        this.menu = menu;
+        this.setProfileIcon();
+        return true;
+    }
+
+    private void setProfileIcon() {
+        if(this.menu == null)
+            return;
+
+        try {
+            Bitmap userPhoto = UserProfile.getUserProfile(getApplicationContext()).getPicture(getApplicationContext());
+            if(userPhoto != null) {
+                BitmapDrawable dra = new BitmapDrawable(getApplicationContext().getResources(), userPhoto);
+                this.menu.findItem(R.id.profile_menu).setIcon(dra);
+            }
+            else {
+                this.menu.findItem(R.id.profile_menu).setIcon(R.mipmap.user_profile_icon);
+            }
+        } catch (Exception e) {
+            Log.e("MAINACTIVITY", "Error setting profile picture. Error:" + e.getMessage());
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
