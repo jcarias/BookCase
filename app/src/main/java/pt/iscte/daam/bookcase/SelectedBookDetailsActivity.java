b14e789995d32d1113cc891770475efc09f7d340
bmso <bmfso31@gmail.com>
2016-05-12 20:52:57 +0100

    Lista de contactos para emprestar livro.

--- app/src/main/java/pt/iscte/daam/bookcase/SelectedBookDetailsActivity.java
+++ app/src/main/java/pt/iscte/daam/bookcase/SelectedBookDetailsActivity.java
@@ -1,10 +1,19 @@
 package pt.iscte.daam.bookcase;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pt.iscte.daam.bookcase.bo.BookCaseDbHelper;
 import pt.iscte.daam.bookcase.bo.GRBook;
@@ -35,4 +44,14 @@ public class SelectedBookDetailsActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.bookAuthorsDetails)).setText(this.book.getAuthors());
       //  ((ImageView) findViewById(R.id.imageProfilePicture)).setImageBitmap(null);
     }
+
+    public void getContacts(View view) {
+
+        String[] fromColumns = { ContactsContract.Contacts.DISPLAY_NAME };
+        int[] toViews = { android.R.id.text1 };
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, fromColumns, toViews, 0);;
+        ((ListView) findViewById(R.id.listviewpersonalcontacts)).setAdapter(adapter);
+    }
+
 }
