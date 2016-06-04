dc844387ca0c8df8b42c07ba7f37d5540fad6e3f
jcarias <jalcs@iscte-iul.pt>
2016-06-04 01:22:44 +0100

    Adicionadas Notifições ao Emprestar/Devolver livro. Tratamento de Leak ao Emprestar Substituição de Strings literais por resources Limpeza e optimizações no Código

--- app/src/main/java/pt/iscte/daam/bookcase/SelectedBookDetailsActivity.java
+++ app/src/main/java/pt/iscte/daam/bookcase/SelectedBookDetailsActivity.java
@@ -12,13 +12,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -37,15 +37,12 @@ import pt.iscte.daam.bookcase.bo.GRBook;
 public class SelectedBookDetailsActivity extends AppCompatActivity {
 
     private GRBook book;
-    private boolean contactsAreListed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.book_item_details);
-
-        this.contactsAreListed = false;
 
         Bundle extras = getIntent().getExtras();
         String bookId = extras.getString("bookApplicationId");
@@ -56,90 +53,132 @@ public class SelectedBookDetailsActivity extends AppCompatActivity {
         if (this.book == null)
             return;
 
-        ((TextView) findViewById(R.id.bookTitleDetails)).setText(this.book.getTitle());
-        ((TextView) findViewById(R.id.bookAuthorsDetails)).setText(this.book.getAuthors());
-        ((TextView) findViewById(R.id.tvYear)).setText(this.book.getReleaseYear());
+        TextView tvBookTitle = (TextView) findViewById(R.id.bookTitleDetails);
+        if (tvBookTitle != null) {
+            tvBookTitle.setText(this.book.getTitle());
+        }
 
-        String ratingLabel = getResources().getString(R.string.ratingLabel);
-        ((TextView) findViewById(R.id.tvRatingLabel)).setText(String.format("%s: (%s/5)", ratingLabel, this.book.getAverageRating()));
-        setRatingStars(this.book);
+        TextView tvBookAuthor = (TextView) findViewById(R.id.bookAuthorsDetails);
+        if (tvBookAuthor != null) {
+            tvBookAuthor.setText(this.book.getAuthors());
+        }
+
+        TextView tvBookYear = (TextView) findViewById(R.id.tvYear);
+        if (tvBookYear != null) {
+            tvBookYear.setText(this.book.getReleaseYear());
+        }
+
+        TextView tvRatingLabel = (TextView) findViewById(R.id.tvRatingLabel);
+        if (tvRatingLabel != null) {
+            String ratingLabel = getResources().getString(R.string.ratingLabel);
+            tvRatingLabel.setText(String.format("%s: (%s/5)", ratingLabel, this.book.getAverageRating()));
+        }
+
+        setRatingStars();
 
         if (this.book.getCoverImage() != null) {
             byte[] image = this.book.getCoverImage();
             Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
-            ((ImageView) findViewById(R.id.coverPhoto)).setImageBitmap(bitmap);
+            ImageView ivCoverPhoto = (ImageView) findViewById(R.id.coverPhoto);
+            if (ivCoverPhoto != null) {
+                ivCoverPhoto.setImageBitmap(bitmap);
+            }
         }
 
-        ((ImageButton) findViewById(R.id.bookDeleteButton)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(v.getContext())
-                        .setTitle("Delete Book")
-                        .setMessage("Are you sure you want to delete this book?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                BookCaseDbHelper bd = new BookCaseDbHelper(getApplicationContext());
-                                bd.deleteBook(book);
+        ImageButton imgBtnDeleteBook = (ImageButton) findViewById(R.id.bookDeleteButton);
+        if (imgBtnDeleteBook != null) {
+            imgBtnDeleteBook.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(v.getContext())
+                            .setTitle("Delete Book")
+                            .setMessage("Are you sure you want to delete this book?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    BookCaseDbHelper bd = new BookCaseDbHelper(getApplicationContext());
+                                    bd.deleteBook(book);
 
-                                Intent intent = new Intent(getApplicationContext(), BookCaseMainActivity.class);
-                                startActivity(intent);
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        })
-                        .setIcon(R.drawable.ic_warning_black_48px)
-                        .show();
-            }
-        });
+                                    Intent intent = new Intent(getApplicationContext(), BookCaseMainActivity.class);
+                                    startActivity(intent);
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // do nothing
+                                }
+                            })
+                            .setIcon(R.drawable.ic_warning_black_48px)
+                            .show();
+                }
+            });
+        }
 
+        Button btnLend = (Button) findViewById(R.id.buttonLentTo);
+        View panelLentTo = findViewById(R.id.panelLentTo);
         if (this.book.getLentTo() != null) {
-            ((Button) (findViewById(R.id.buttonLentTo))).setText("Retake book");
+            if (btnLend != null) {
+                btnLend.setText(R.string.btn_label_Retake_Book);
+            }
 
-            ((LinearLayout) findViewById(R.id.panelLentTo)).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.tvLentTo)).setText(this.book.getLentTo());
-            ((TextView) findViewById(R.id.tvLentToDate)).setText(this.book.getLentToDate());
+            if (panelLentTo != null) {
+                panelLentTo.setVisibility(View.VISIBLE);
+            }
+
+            TextView tvLentTo = (TextView) findViewById(R.id.tvLentTo);
+            if (tvLentTo != null) {
+                tvLentTo.setText(this.book.getLentTo());
+            }
+
+            TextView tvLentToDate = (TextView) findViewById(R.id.tvLentToDate);
+            if (tvLentToDate != null) {
+                tvLentToDate.setText(this.book.getLentToDate());
+            }
         } else {
-            ((TextView) findViewById(R.id.lentToTextBox)).setText("Book available.");
-            ((LinearLayout) findViewById(R.id.panelLentTo)).setVisibility(View.GONE);
+            if (panelLentTo != null) {
+                panelLentTo.setVisibility(View.GONE);
+            }
         }
 
-        (findViewById(R.id.buttonLentTo)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (book.getLentTo() != null) {
-                    BookCaseDbHelper bd = new BookCaseDbHelper(getApplicationContext());
-                    bd.lentBookTo(book, null, null);
+        if (btnLend != null) {
+            btnLend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (book.getLentTo() != null) {
+                        BookCaseDbHelper bd = new BookCaseDbHelper(getApplicationContext());
+                        bd.lentBookTo(book, null, null);
 
-                    finish();
-                    startActivity(getIntent());
+                        finish();
+                        startActivity(getIntent());
 
-                    return;
+                        String message = String.format(getResources().getString(R.string.message_book_returned), book.getTitle());
+                        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+                        toast.show();
+
+                        return;
+                    } else {
+
+                        //Carrega a lista de Contactos e Mostra o dialog
+                        (new LoadContactsAyscn()).execute();
+
+                    }
                 }
+            });
+        }
 
-                if (!contactsAreListed) {
-                    contactsAreListed = true;
-                    (new LoadContactsAyscn()).execute();
-                } else {
-                    contactsAreListed = false;
-                }
 
-            }
-        });
-
-        if (extras.getInt("NewBook") == 1){
+        if (extras.getInt("NewBook") == 1) {
             Context context = getApplicationContext();
             Toast toast = Toast.makeText(context, "Book Added!", Toast.LENGTH_LONG);
             toast.show();
         }
     }
 
-    private void setRatingStars(GRBook book) {
+    private void setRatingStars() {
         RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBookBar);
-        Float rating = new Float(this.book.getAverageRating());
-        ratingBar.setRating(rating);
+        if (ratingBar != null) {
+            Float rating = Float.valueOf(this.book.getAverageRating());
+            ratingBar.setRating(rating);
+        }
     }
 
     private class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
@@ -147,31 +186,30 @@ public class SelectedBookDetailsActivity extends AppCompatActivity {
         @Override
         protected ArrayList<String> doInBackground(Void... params) {
 
-            ArrayList<String> contactosAL = new ArrayList<String>();
+            ArrayList<String> contactosAL = new ArrayList<>();
 
-            Cursor c = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
-            while (c.moveToNext()) {
+            Cursor c = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+            while (c != null && c.moveToNext()) {
                 String contactName = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                 if (contactosAL.contains(contactName))
                     continue;
                 contactosAL.add(contactName);
             }
-            c.close();
+            if (c != null) {
+                c.close();
+            }
 
             return contactosAL;
         }
 
         @Override
         protected void onPostExecute(ArrayList<String> contactosAL) {
-            super.onPostExecute(contactosAL);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.contact_list_item, contactosAL);
 
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.contact_list_item, contactosAL);
-
-
-            Dialog dialog = new Dialog(SelectedBookDetailsActivity.this);
+            final Dialog dialog = new Dialog(SelectedBookDetailsActivity.this);
             dialog.setContentView(R.layout.contacts_list);
 
-            ListView lv = (ListView ) dialog.findViewById(R.id.lv);
+            ListView lv = (ListView) dialog.findViewById(R.id.lv);
             lv.setAdapter(adapter);
             dialog.setCancelable(true);
             dialog.setTitle(R.string.pick_contact_title);
@@ -180,8 +218,7 @@ public class SelectedBookDetailsActivity extends AppCompatActivity {
             dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
                 @Override
                 public void onCancel(DialogInterface dialog) {
-                    finish();
-                    startActivity(getIntent());
+                    dialog.dismiss();
                 }
             });
 
@@ -190,12 +227,16 @@ public class SelectedBookDetailsActivity extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     String contact = adapter.getItem(position);
-
                     BookCaseDbHelper bd = new BookCaseDbHelper(getApplicationContext());
                     bd.lentBookTo(book, contact, new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
-
+                    dialog.dismiss();
                     finish();
                     startActivity(getIntent());
+
+                    String message = String.format(getResources().getString(R.string.message_book_lent), book.getTitle(), contact);
+                    Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
                 }
             });
 
